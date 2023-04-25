28d44316c6aabe43095affd5122c357ac6b0c86e
Mukhriddin <doctor-maxin@yandex.ru>
2023-04-25 22:53:02 +0700

    -

--- src/components/footer/FooterSubscribe.vue
+++ src/components/footer/FooterSubscribe.vue
@@ -1,1 +1,12 @@
-<template><div>Hello</div></template>
+<script setup lang="ts">
+import Button from '../ui/Button.vue';
+</script>
+<template>
+<div>
+    <div class="caption-bold text-white mb-4">Получайте рекомендации и выгодные предложения на почту</div>
+    <form class="flex max-h-12">
+        <input placeholder="E-mail" class="h-full bg-white px-5 py-3 border-white border -mr-4 rounded-xx" />
+        <Button variant="secondary">Подписаться </Button>
+    </form>
+</div>
+</template>
